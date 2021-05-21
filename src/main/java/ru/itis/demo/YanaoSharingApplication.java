cc8844b5e133244ebd693bd3d5cca202a5960d83
Rifdar <assasinnyx@mail.ru>
2021-05-21 18:52:16 +0300

    Initial commit

---
+++ src/main/java/ru/itis/demo/YanaoSharingApplication.java
@@ -0,0 +1,13 @@
+package ru.itis.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YanaoSharingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YanaoSharingApplication.class, args);
+    }
+
+}
