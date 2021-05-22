be6e1d83850db34a84ff6ad03cafd028c6efa3ac
KoaLa1101 <rproba@list.ru>
2021-05-22 17:48:21 +0300

    add additional of marker

--- src/main/java/ru/itis/demo/controller/SalePointSearchController.java
+++ src/main/java/ru/itis/demo/controller/SalePointSearchController.java
@@ -1,15 +1,16 @@
 package ru.itis.demo.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
 import ru.itis.demo.model.dto.CriterySearchDto;
 import ru.itis.demo.service.SearchService;
 
 @Controller
+@Slf4j
 public class SalePointSearchController {
 
     @Autowired
@@ -17,5 +18,11 @@ public class SalePointSearchController {
     @GetMapping("/getpoints")
     public ResponseEntity<?> getSalePoints(@ModelAttribute("criterySearchDto") CriterySearchDto criterySearchDto){
      return null;
+    }
+
+    @RequestMapping("/showPointOfSale")
+    public String showPointOfSale(@RequestParam("pointOfSaleId")Long pointOfSaleId, Model model){
+        model.addAttribute("pointOfSale", searchService.getPointOfSale(pointOfSaleId).get());
+        return "pointOfSale";
     }
 }
--- src/main/java/ru/itis/demo/service/SearchService.java
+++ src/main/java/ru/itis/demo/service/SearchService.java
@@ -2,10 +2,12 @@ package ru.itis.demo.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import ru.itis.demo.model.PointOfSale;
 import ru.itis.demo.model.SportInventory;
+import ru.itis.demo.repository.SalePointsRepository;
 import ru.itis.demo.repository.SportIventoryRepository;
 
-import java.util.List;
+import java.util.Optional;
 import java.util.Set;
 import java.util.stream.Collectors;
 
@@ -14,10 +16,16 @@ public class SearchService {
 
     @Autowired
     private SportIventoryRepository sportIventoryRepository;
+    @Autowired
+    private SalePointsRepository salePointsRepository;
     public void searchForCriteria(){
 
     }
     public Set<String> findAllNameOfItems(){
         return sportIventoryRepository.findAll().stream().map(SportInventory::getNameOfItem).collect(Collectors.toSet());
+    }
+
+    public Optional<PointOfSale> getPointOfSale(Long id){
+        return salePointsRepository.findById(id);
     }
 }
