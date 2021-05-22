6f2300aa5c2988f504a22544f0c35d2eabb9ee34
Damir D <damir180501@mail.ru>
2021-05-22 14:30:26 +0300

    Added Index Page

--- src/main/java/ru/itis/demo/controller/MainPageController.java
+++ src/main/java/ru/itis/demo/controller/MainPageController.java
@@ -3,15 +3,22 @@ package ru.itis.demo.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import ru.itis.demo.service.LandLordService;
 
 @Controller
 public class MainPageController {
     @Autowired
     private LandLordService landLordService;
+
     @GetMapping("/yanaosharing")
-    public String getMainPage(){
+    public String getMainPage() {
         landLordService.saveLandlordWithPointOfSale();
         return "mainPage";
+    }
+
+    @RequestMapping("/")
+    public String index() {
+        return "index";
     }
 }
