62408f693bf5408b2774277616a8ccd22da2ab00
Rifdar <assasinnyx@mail.ru>
2021-05-22 14:53:38 +0300

    added saving and updating savepoints

--- src/main/java/ru/itis/demo/controller/PointGiverController.java
+++ src/main/java/ru/itis/demo/controller/PointGiverController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 import ru.itis.demo.service.PointOfSaleService;
 
 @RestController
-public class UserGiverController {
+public class PointGiverController {
     @Autowired
     private PointOfSaleService pointOfSaleService;
     @GetMapping("/getPoints")
--- src/main/java/ru/itis/demo/service/PointOfSaleService.java
+++ src/main/java/ru/itis/demo/service/PointOfSaleService.java
@@ -2,10 +2,14 @@ package ru.itis.demo.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import ru.itis.demo.model.Landlord;
 import ru.itis.demo.model.PointOfSale;
 import ru.itis.demo.repository.SalePointsRepository;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityNotFoundException;
+import javax.persistence.EntityTransaction;
 import java.util.List;
 
 @Service
@@ -13,7 +17,27 @@ public class PointOfSaleService {
     @Autowired
     SalePointsRepository salePointsRepository;
 
-    public void addTestSalePoint(){
+    @Autowired
+    EntityManager entityManager;
+    @Transactional
+    public void addTestSalePoint(PointOfSale point){
+
+      salePointsRepository.save(point);
+
+
+    }
+
+    @Transactional
+    public void updateSalePointById(PointOfSale salePoint){
+
+       PointOfSale point =salePointsRepository.findById(salePoint.getPointOfSaleId()).orElseThrow(EntityNotFoundException::new);
+       point.setDescription(salePoint.getDescription());
+       point.setLandlord(salePoint.getLandlord());
+       point.setNameOfOrganization(salePoint.getNameOfOrganization());
+       point.setPositionX(salePoint.getPositionX());
+       point.setPositionY(salePoint.getPositionY());
+       point.setSportInventoryList(salePoint.getSportInventoryList());
+       salePointsRepository.save(salePoint);
 
     }
     public List<PointOfSale> findAll(){
